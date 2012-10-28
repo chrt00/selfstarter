a8d4828de1df747b6b003096986ebdd0e9ceb669
Chris To <chrt00@gmail.com>
2012-10-28 07:07:08 -0700

    added amazon access key config via ENV for production

--- config/initializers/amazon_flexible_payments.rb
+++ config/initializers/amazon_flexible_payments.rb
@@ -1,3 +1,8 @@
-AmazonFlexPay.access_key = Settings.amazon_access_key
-AmazonFlexPay.secret_key = Settings.amazon_secret_key
+if Rails.env.production?
+  AmazonFlexPay.access_key = ENV['AMAZON_ACCESS_KEY']
+  AmazonFlexPay.secret_key = ENV['AMAZON_SECRET_KEY']
+else
+  AmazonFlexPay.access_key = Settings.amazon_access_key
+  AmazonFlexPay.secret_key = Settings.amazon_secret_key
+end
 AmazonFlexPay.go_live! if Rails.env.production?
