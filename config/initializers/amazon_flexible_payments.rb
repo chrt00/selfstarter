2fcd1f0023683382e654db6e770ec3f0910ec1ab
Chris To <chrt00@gmail.com>
2012-10-28 08:31:31 -0700

    fix error with amazon FPS sandbox init

--- config/initializers/amazon_flexible_payments.rb
+++ config/initializers/amazon_flexible_payments.rb
@@ -1,7 +1,6 @@
 if Rails.env.production?
   AmazonFlexPay.access_key = ENV['AMAZON_ACCESS_KEY']
   AmazonFlexPay.secret_key = ENV['AMAZON_SECRET_KEY']
-  AmazonFlexPay.go_live! = false
 else
   AmazonFlexPay.access_key = Settings.amazon_access_key
   AmazonFlexPay.secret_key = Settings.amazon_secret_key
